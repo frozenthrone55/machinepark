6eb2d7222c7ec1b84067defbc950d2d8d040b6ce
frozenthrone55 <33559269+frozenthrone55@users.noreply.github.com>
2026-08-25 20:32:38 +0200

    Cache Machinepark logo in development

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='machinepark-v1.23';
-const ASSETS=['/','/index.html','/manifest.webmanifest'];
+const CACHE='machinepark-v1.23-brand';
+const ASSETS=['/','/index.html','/manifest.webmanifest','/machinepark-logo.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{
