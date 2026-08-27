7b165ee01046474ffa0f03defe50d0c50d7a6540
frozenthrone55 <33559269+frozenthrone55@users.noreply.github.com>
2026-08-27 23:22:21 +0200

    Vernieuw cache na herstel van navigatie

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='machinepark-v1.52-professional-foundation';
+const CACHE='machinepark-v1.53-navigation-fix';
 const ASSETS=['/','/index.html','/manifest.webmanifest','/machinepark-logo.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
